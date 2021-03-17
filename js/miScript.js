db0b8add99be7a43dc52e45161ea4a6ca1637a64
ignacio Soria <soria.ign@gmail.com>
2021-03-16 21:36:47 -0300

    agregar y eliminar productos funciona

--- js/miScript.js
+++ js/miScript.js
@@ -1,56 +1,112 @@
-// ARRAY DE OBJETOS: PRODUCTOS
-let misProductos = [{productoid: 1, nombre: "Producto 1", stock: 12, precio: 780},
-{productoid: 2, nombre: "Producto 2", stock: 12, precio: 620},
-{productoid: 3, nombre: "Producto 3", stock: 12, precio: 500},
-{productoid: 4, nombre: "Producto 4", stock: 12, precio: 500},
-{productoid: 5, nombre: "Producto 5", stock: 12, precio: 450}]
 
-//SELECCIONAR PRODUCTO: ESTO VINCULADO A LOS BOTONES DE AGREGAR PRODUCTO
-let agregarProducto1 = document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[0].nombre+" - $ "+misProductos[0].precio+"</a></li>",
-    agregarProducto2 = document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[1].nombre+" - $ "+misProductos[1].precio+"</a></li>",
-    agregarProducto3 = document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[2].nombre+" - $ "+misProductos[2].precio+"</a></li>",
-    agregarProducto4 = document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[3].nombre+" - $ "+misProductos[3].precio+"</a></li>",
-    agregarProducto5 = document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[4].nombre+" - $ "+misProductos[4].precio+"</a></li>";
+(function(){
+
+// VARIABLES
+let lista = document.getElementById("lista"),
+    lista2 = document.getElementById("lista2");
+
+// ARRAY DE OBJETOS: PRODUCTOS
+let misProductos = [{productoId: 1, nombre: "Producto 1", stock: 12, precio: 780},
+{productoId: 2, nombre: "Producto 2", stock: 12, precio: 620},
+{productoId: 3, nombre: "Producto 3", stock: 12, precio: 500},
+{productoId: 4, nombre: "Producto 4", stock: 12, precio: 500},
+{productoId: 5, nombre: "Producto 5", stock: 12, precio: 450}]
 
 // ARRAY DE OBJETOS: PRESENTACION  
 let presentacion = [{tipo: "Bolsa", cantidad: 1},
                     {tipo: "Caja x 2", cantidad : 2},
                     {tipo: "Caja x 3", cantidad : 3}];
 
-// ELEGIR LA PRESENTACION: ESTO VINCULADO A LAS IMAGENES DE ELEGIR LA PRESENTACION 
-let presentacion1 = document.getElementById("lista2").innerHTML += "<li><a href='#'>"+presentacion[0].tipo+"</a></li>",
-    presentacion2 = document.getElementById("lista2").innerHTML += "<li><a href='#'>"+presentacion[1].tipo+"</a></li>",
-    presentacion3 = document.getElementById("lista2").innerHTML += "<li><a href='#'>"+presentacion[2].tipo+"</a></li>";
+// AGREGAR PRODUCTOS AL LISTA
+let producto1 = document.getElementById("producto1");
+        producto1.addEventListener("click", function producto1() {
+            document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[0].nombre+" - $ "+misProductos[0].precio+"</a></li>";
+            for (let i = 0; i <= lista.children.length -1; i++) {
+                lista.children[i].addEventListener("click", eliminarProducto);   
+            }
+        })
 
-// TOTAL SIN DESCUENTO
-let totalParcial 
-let total = document.getElementById("totalPrecio");
-// CALCULAR TOTAL 
-if (presentacion.cantidad == 2) {
-    let total = totalParcial * 0.9;
-    alert(total);        
-    } else {
-    if (presentacion.cantidad == 3) {
-      let total = totalParcial * 0.85;
-        alert(total);        
-        } else {
-            let = total = totalParcial;
-            alert(total);
-    }
+let producto2 = document.getElementById("producto2");
+        producto2.addEventListener("click", function producto2() {
+            document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[1].nombre+" - $ "+misProductos[1].precio+"</a></li>";
+            for (let i = 0; i <= lista.children.length -1; i++) {
+                lista.children[i].addEventListener("click", eliminarProducto);   
+            }
+        })
+let producto3 = document.getElementById("producto3");
+        producto3.addEventListener("click", function producto3() {
+            document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[2].nombre+" - $ "+misProductos[2].precio+"</a></li>";            
+            for (let i = 0; i <= lista.children.length -1; i++) {
+                lista.children[i].addEventListener("click", eliminarProducto);   
+            }
+        })
+let producto4 = document.getElementById("producto4");
+        producto4.addEventListener("click", function producto4() {
+            document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[3].nombre+" - $ "+misProductos[3].precio+"</a></li>";
+            for (let i = 0; i <= lista.children.length -1; i++) {
+                lista.children[i].addEventListener("click", eliminarProducto);   
+            }
+        })
+let producto5 = document.getElementById("producto5");
+        producto5.addEventListener("click", function producto5() {
+            document.getElementById("lista").innerHTML += "<li><a href='#'>"+misProductos[4].nombre+" - $ "+misProductos[4].precio+"</a></li>";
+            for (let i = 0; i <= lista.children.length -1; i++) {
+                lista.children[i].addEventListener("click", eliminarProducto);   
+            }
+        })
+
+
+// ELEGIR LA PRESENTACION
+let presentacion1 = document.getElementById("presentacion1");
+        presentacion1.addEventListener("click", function presentacion1() {
+            document.getElementById("lista2").innerHTML += "<li><a href='#'>"+presentacion[0].tipo+"</a></li>";
+            for (let i = 0; i <= lista2.children.length -1; i++) {
+                lista2.children[i].addEventListener("click", eliminarPresentacion);    
+            }
+        })
+let presentacion2 = document.getElementById("presentacion2");
+        presentacion2.addEventListener("click", function presentacion2() {
+            document.getElementById("lista2").innerHTML += "<li><a href='#'>"+presentacion[1].tipo+"</a></li>";
+            for (let i = 0; i <= lista2.children.length -1; i++) {
+                lista2.children[i].addEventListener("click", eliminarPresentacion);    
+            }
+        })
+let presentacion3 = document.getElementById("presentacion3");
+        presentacion3.addEventListener("click", function presentacion3() {
+            document.getElementById("lista2").innerHTML += "<li><a href='#'>"+presentacion[2].tipo+"</a></li>";            
+            for (let i = 0; i <= lista2.children.length -1; i++) {
+                lista2.children[i].addEventListener("click", eliminarPresentacion);    
+            }
+        })
+
+// CALCULAR TOTAL
+let totalPrecio = document.getElementById("totalPrecio");
+
+//FINALIZAR COMPRA
+function finalizarCompra() {
+    alert("Finalizar compra");
 }
 
-// BOTON PARA FINALIZAR COMPRA 
-let finalizarCompra = function () {};
-
 // ELIMINAR PRODUCTO
-let eliminarProducto = function () {
-    document.getElementById("lista2").children[0].remove()
-};
+let eliminarProducto = function eliminarProducto() {
+    this.parentNode.removeChild(this);
+}
+for (let i = 0; i <= lista.children.length -1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);   
+}
 
-// CICLO FOR PARA RECORRER LOS ARRAYS
+// ELIMINAR PRESENTACION
+let eliminarPresentacion = function eliminarPresentacion() {
+    this.parentNode.removeChild(this);
+}
+for (let i = 0; i <= lista2.children.length -1; i++) {
+    lista2.children[i].addEventListener("click", eliminarPresentacion);    
+}
 
 
-// Eventos
+
+
+}());
 
 // 1: Seleccionar un articulo y que este se agregue automaticamente a una lista mediante boton agregar producto.
 // 2: Sumar el total de los valores de los productos seleccionados.
---
+++ js/misObjetos.js
@@ -0,0 +1,8 @@
+// FUNCION CONSTRUCTORA: PRODUCTOS
+function misProductos(productoId, nombre, stock, precio) {
+    this.productoId; 
+    this.nombre; 
+    this.stock; 
+    this.precio;  
+    
+};
